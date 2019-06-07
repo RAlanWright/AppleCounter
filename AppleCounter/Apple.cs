7614701576287e23901cc1f41381a7db676b3929
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-07 16:33:05 -0500

    Created Apple class; Set up program to take in user input for the number of apples; Output the total cost of the apples

---
+++ AppleCounter/Apple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleCounter
+{
+    class Apple
+    {
+        public string name;
+        public double price;
+        public int number;
+
+        public Apple(string aName, double aPrice, int aNumber)
+        {
+            name = aName;
+            price = aPrice;
+            number = aNumber;
+        }
+    }
+}
--- AppleCounter/Program.cs
+++ AppleCounter/Program.cs
@@ -8,8 +8,56 @@ namespace AppleCounter
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+
+            Console.WriteLine(new string('*', 100));
+            Console.WriteLine("This program will take in a number and calculate the price for" +
+                " the number of apples you enter in.");
+            Console.WriteLine(new string('*', 100));
+
+            Console.Write("How many apples would you like? ");
+            Console.WriteLine();
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 100));
+            Console.WriteLine(new string('*', 100));
+
+
+
+            Apple granny = new Apple("Granny Smith", 1.49, count);
+            Apple fuji = new Apple("Fuji", 1.56, count);
+
+
+            Console.WriteLine();
+            Console.WriteLine("The price of {0} {1} apples would be: ${2}.", count, granny.name, applePrice(granny.name, count, granny.price));
+            Console.WriteLine();
+
+            Console.WriteLine(new string('*', 100));
+
+            Console.WriteLine();
+            Console.WriteLine("The price of {0} {1} apples would be: ${2}.", count, fuji.name, applePrice(fuji.name, count, fuji.price));
+            Console.WriteLine();
+
+            Console.WriteLine(new string('*', 100));
+
+            Console.ReadLine();
+        }
+        public static double applePrice(string name, int count, double price)
+        {
+            double total = 0.0;
+            if (name == "Granny Smith")
+            {
+                total = count * price;
+
+                return total;
+            }
+            else
+            {
+                total = count * price;
+                return total;
+            }
         }
     }
 }
